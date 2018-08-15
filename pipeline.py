c82cdb3290a90655725b5a2e9c38d8460819b506
Bijan Chokoufe Nejad <bijan.chokoufe@esailors.de>
2018-08-15 10:22:16 +0200

    Fix and refactor calibration

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,7 @@
-undistort
-null out edges (select trapezoidal region)
-thresholding
-perspective transform
-lane detect
-inverse perspective transform and plot
+def main():
+  undistort()
+  perspective_transform()
+  null_out_edges() # (select trapezoidal region)
+  thresholding()
+  lane_detect()
+  inverse_perspective_transform_and_plot()
